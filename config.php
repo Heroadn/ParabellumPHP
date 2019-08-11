50ff8d0d31bc1f014ca6db902b003479f387be15
Benjamin de Castro Azevedo P <36571620+Heroadn@users.noreply.github.com>
2019-08-11 18:48:12 -0300

    aquivo de configuração adicionado

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+#Definindo o template e pastas CSS e JS
+define('TEMPLATE_SELECTED','Bootstrap');
+define('DEFAULT_PAGE','/Usuario/Cadastrar');
+
+#Recursos CSS, JS e Imagens
+define('CSS' , LIBS .  'css' . DIRECTORY_SEPARATOR);
+define('JS'  , LIBS .  'js'  . DIRECTORY_SEPARATOR);
+define('IMG' , LIBS .  'img' . DIRECTORY_SEPARATOR);
+
+#Banco de dados
+define('DATABASE','#nome_db');
+define('HOST','#nome_host');
+define('USER','#nome_dbUser');
+define('PASS','#senha_db');
+
+#Segurança
+define('SALT',md5('1ff2Sh@aj33f2&#&3Ssf%goa$s1'));
+define('SECURE',false);
+define('DEBUG' ,true);

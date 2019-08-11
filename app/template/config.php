027a099290ab98a497cf34130a836674a267c9d9
Benjamin de Castro Azevedo P <36571620+Heroadn@users.noreply.github.com>
2019-08-11 20:22:39 -0300

    Create config.php

---
+++ app/template/config.php
@@ -0,0 +1,23 @@
+<?php
+
+##Inicialização de CSS e JS
+define('CSS_HEADER',serialize([
+    'bootstrap.min.css',
+    'bootstrap-grid.min.css',
+    'fw.css',
+    'core.css',
+    'color.css',
+    'balloon.css',
+    'preload.css',
+    'style.css',
+    'highlighted.css'
+]));
+
+define('JS_HEADER',serialize([
+    'jquery.js',
+    'bootstrap.min.js',
+    'serializejson.js',
+    'easter_eggs.js',
+    'themes.js',
+    'popper.min.js'
+]));
